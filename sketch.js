ffbe502ede314ba30eee5d8122671fec4db58304
WilliamBrody <william.brody27@trinityschoolnyc.org>
2024-04-12 10:04:57 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -12,7 +12,7 @@ let enemy;
 let block;
 let healthBarImg;
 let bar;
-let gameState = false;;
+let gameState = false;
 let dashCooldown;
 let highScore;
 let reset;
@@ -22,19 +22,22 @@ let jump = false
 let fallDist = -3
 let fall = false
 let ground = 400
-let enemyArr = ["Flamingo", "Giraffe"];
+let enemyArr = ["flamingo", "giraffe", "giraffe"];
 let easyButton;
 let normalButton;
 let hardButton;
-let enemySizeX = [120,200]
-let enemySizeY = [120,200]
+let enemySizeX = [120,200,350]
+let enemySizeY = [120,200,350]
 let damage = [
   [0.5, 1, 2.5], //flamingo
-  [1, 2.5, 5] //giraffe
+  [1, 2.5, 5], //giraffe,   
+  [2, 3, 6]
 ]
 let mode;
-let mugX = [25, 525];
-let mugY = [350, 50];
+let mugX = [25, 525, 25];
+let mugY = [350, 50, 350];
+let spikeArr = [];
+let spike;
 
 function setup() {
   createCanvas(600, 400);
@@ -68,31 +71,44 @@ function setup() {
 
 }
 
+
 function preload() {
   levelMaps.push(loadStrings("level1.txt"));
   levelMaps.push(loadStrings("level2.txt"));
+  levelMaps.push(loadStrings("level3.txt"));
   mugWalk.push(loadImage('mugMove/mug1.png'), loadImage('mugMove/mug2.png'), loadImage('mugMove/mug3.png'));
   enemyMove.push(loadImage(enemyArr[level]+'Move/'+enemyArr[level]+'1.png'), loadImage(enemyArr[level]+'Move/'+enemyArr[level]+'2.png'), loadImage(enemyArr[level]+'Move/'+enemyArr[level]+'3.png'));
   enemyAttack.push(loadImage(enemyArr[level]+'Attack/'+enemyArr[level]+'1.png'), loadImage(enemyArr[level]+'Attack/'+enemyArr[level]+'2.png'));
   mugAttackImg = loadImage('mugAttack.png');
   block = loadImage('block.png');
+  spike = loadImage('spike.png')
   bg = loadImage('background.png')
 }
 
-function reload(level) {
-  enemyMove= []
-  enemyAttack=[]
-  enemyMove.push(loadImage(enemyArr[level]+'Move/'+enemyArr[level]+'1.png'), loadImage(enemyArr[level]+'Move/'+enemyArr[level]+'2.png'), loadImage(enemyArr[level]+'Move/'+enemyArr[level]+'3.png'));
-  enemyAttack.push(loadImage(enemyArr[level]+'Attack/'+enemyArr[level]+'1.png'), loadImage(enemyArr[level]+'Attack/'+enemyArr[level]+'2.png'));
-  mug = new Mug(50, 350);
-  dashCooldown = 0;
-  enemy = new Enemy(300, 200);
-  blocks = loadLevel(levelMaps[level]);
-  bar = new healthBar();
-  score = 0;
-  coinArr.push(new Coin(560, 290, 10));
-  reset.hide();
-  gameState = true;
+function reload() {
+  if(level==3){
+      level = 0;
+      reload();
+  }else{
+    enemyMove = []
+    enemyAttack = []
+    enemyMove.push(loadImage(enemyArr[level]+'Move/'+enemyArr[level]+'1.png'), loadImage(enemyArr[level]+'Move/'+enemyArr[level]+'2.png'), loadImage(enemyArr[level]+'Move/'+enemyArr[level]+'3.png'));
+    enemyAttack.push(loadImage(enemyArr[level]+'Attack/'+enemyArr[level]+'1.png'), loadImage(enemyArr[level]+'Attack/'+enemyArr[level]+'2.png'));
+    
+    mug = new Mug(mugX[level], mugY[level]);//reset position based on level
+    dashCooldown = 0;
+    enemy = new Enemy(300, 400-enemySizeY[level]);
+    print(enemySizeY[level])
+    print(400-enemySizeY[level])
+    blocks = loadLevel(levelMaps[level]);
+    spikeArr = loadSpikes(levelMaps[level]);
+    bar = new healthBar();
+    score = 0;
+    if (mug.health <= 0) level = 0;
+    coinArr.push(new Coin(560, 290, 10));
+    reset.hide();
+    gameState = true;
+  }
 }
 
 function easyMode() {
@@ -123,8 +139,11 @@ function draw() {
 
 
   background(220);
-
-  if (gameState) {
+  if(level==3){
+    text("          Congratulations!\n\nclick reset to restart the game", 225, 150);
+    reset.show();
+  }
+  if (gameState && level!=3) {
     image(bg, 0, 0);
     bar.show();
     if (mug.health <= 0) gameState = false;
@@ -142,7 +161,10 @@ function draw() {
     enemy.move();
     for (let i = 0; i < blocks.length; i++) {
       block.resize(blocks[i].w, blocks[i].h);
-      image(block, blocks[i].x, blocks[i].y);
+      spike.resize(blocks[i].w, blocks[i].h)
+      let drawImg = block;
+      if (blocks[i].type == "Spike") drawImg = spike;
+      image(drawImg, blocks[i].x, blocks[i].y);
     }
     for (let i = 0; i < coinArr.length; i++){
       coinArr[i].show();
@@ -155,7 +177,9 @@ function draw() {
     enemy.draw();
     if (enemy.alive == false){
       level ++;
-      reload(level);
+      if(level!=3){
+        reload();
+      }
     }
   } else if (mug.health <= 0) {
     
@@ -178,6 +202,8 @@ function loadLevel(arr) {
     for (let z = 0; z < temp.length; z++) {
       if (temp[z] == "+") {
         obj.push(new Bound(x, y, deltaX, deltaY));
+      } else if (temp[z] == "^") {
+        obj.push(new Bound(x, y, deltaX, deltaY, "Spike"));
       }
       x += deltaX;
     }
@@ -185,6 +211,27 @@ function loadLevel(arr) {
     x = 0;
   }
   return obj;
+}
+
+function loadSpikes(arr) {
+  spikes = [];
+  let x = 0;
+  let y = 0;
+  let deltaX = 600 / arr[0].length;
+  let deltaY = 10
+  for (let i = 0; i<arr.length; i++) {
+    let line = arr[i].split("");
+    for (let j = 0; j<line.length; j++) {
+      let character = line[j];
+      if (character = "^") {
+        spikes.push(new Bound(x, y, deltaX, deltaY));
+      }
+      x += deltaX
+    }
+    y += deltaY
+    x = 0;
+  }
+  return spikes;
 }
 
 class Mug {
@@ -202,70 +249,91 @@ class Mug {
       this.loc.x = 550;
     }
     if (!this.attacking) {
-      if (keyIsDown(82) && dashCooldown == 0) {
+      if (keyIsDown(82) && dashCooldown == 0) {  //R: dash
         let dashSpeed=200
         for (let i = 0; i < blocks.length; i++) {
-          // while (blocks[i].collide(this.loc.x, this.loc.y, 50, 50)) {
-          //   this.loc.x -= this.deltaX;
-          // }
-          if (blocks[i].y + blocks[i].h == ground && this.loc.x >= blocks[i].x - 200) {
-             dashSpeed=blocks[i].x-this.loc.x-52  
+          if (blocks[i].collide(this.loc.x+200, this.loc.y, 50, 50)) {
+            dashSpeed = blocks[i].x-this.loc.x-50
+            console.log('where',dashSpeed)
+          }
+          if (blocks[i].y + blocks[i].h == ground && this.loc.x+50<blocks[i].x && this.loc.x >= blocks[i].x - 200 && this.deltaX==1 ) {
+            dashSpeed=blocks[i].x-this.loc.x-52  
              break
           }
+        
+          if(blocks[i].y + blocks[i].h == ground && this.loc.x>blocks[i].x+blocks[i].w && this.loc.x <= blocks[i].x+blocks[i].w + 200 && this.deltaX==-1 ) {
+            dashSpeed=this.loc.x-blocks[i].x-blocks[i].w-2
+             break
+          }
+          
         } 
         dashCooldown = 5;
-        if (this.deltaX == 1) this.loc.x += dashSpeed;
-        else this.loc.x -= dashSpeed;
+        console.log(this.deltaX,this.loc.x,dashSpeed)
+        this.loc.x += dashSpeed * this.deltaX
+        console.log(this.deltaX,this.loc.x,dashSpeed)
         
-      } else if (keyIsDown(68) && this.loc.x < 550) {
+      } else if (keyIsDown(68) && this.loc.x < 550) {  //D: right
         this.deltaX = 1
         this.loc.x += this.deltaX;
         if (frameCount % 10 == 0) this.currentFrame += 1;
-      } else if (keyIsDown(65) && this.loc.x > 0) {
+      } else if (keyIsDown(65) && this.loc.x > 0) {   //A:left
         this.deltaX = -1
         this.loc.x += this.deltaX;
         if (frameCount % 10 == 0) this.currentFrame += 1;
       }
+      let m = 0; 
       for (let i = 0; i < blocks.length; i++) {
-        block.resize(blocks[i].w, blocks[i].h);
-        image(block, blocks[i].x, blocks[i].y);
         if (blocks[i].collide(this.loc.x, this.loc.y, 50, 50)) {
-            // if (this.loc.x < blocks[i].x) {
-              // Block is to the right of mug, move mug to the left
-              if (mug.loc.y + 50 != blocks[i].y && keyIsDown(68)) 
-              this.loc.x -= this.deltaX;
-            if (mug.loc.y+50 != blocks[i].y && keyIsDown(65)) {
-              this.loc.x -= this.deltaX
-            } //NEW
-              else
-              ground = blocks[i].y
-              if (keyIsDown(87)){
-                this.loc.y -= 1;
-              }
-           
+          
+          if (blocks[i].type == "Spike") {
+            this.health -= 1;
+            this.loc.x = 25;
+            this.loc.y = 350;
+            jump=false
+            fall=false
+            this.attacking=false
+          }
+          if (mug.loc.y + 50 != blocks[i].y && keyIsDown(68)) 
+            this.loc.x -= this.deltaX;//to counteract walking to right normally
+          else if(mug.loc.y + 50 == blocks[i].y){
+            ground = blocks[i].y
+          }
+
+          if (mug.loc.y+50 != blocks[i].y && keyIsDown(65)) {
+            this.loc.x -= this.deltaX;//to counteract walking to right normally
+          } else if(mug.loc.y + 50 == blocks[i].y){
+            ground = blocks[i].y
+          }
+          
+          if (keyIsDown(87) && !jump && !fall){
+            this.loc.y -= 1;
+          }
+        }
+
+        if (blocks[i].collide(this.loc.x, this.loc.y, 50, 50)||blocks[i].collide(this.loc.x, this.loc.y, 51, 51)){
+          m++;
         }
       }
-      
+      if(m==0 && !jump && !fall && this.loc.y + 50 != 400){
+        fall = true;
+      }
       if (fall) {
-        print("fall")
         for (let i=0; i<blocks.length; i++) {
-        if (blocks[i].collide(this.loc.x, this.loc.y, 50+1, 50+1)) {//NEW
-          print("collide")
-          if (keyIsDown(87)) {
-            print('falling-climb')
-            this.loc.y -= 1
-            fall=false
-          }
-            
-            
-            
-        } else {
+          if (blocks[i].collide(this.loc.x, this.loc.y, 50+1, 50+1)) {//NEW
+            //print("collide")
+            if (keyIsDown(87)) {
+              print('falling-climb')
+              this.loc.y -= 1
+              fall=false
+            } 
+          } else {
+            print("falling")
             this.loc.y += 1
-              if (this.loc.y > 350) {
-                fall=false
-                this.loc.y=350
-                ground=400
-              }
+            if (this.loc.y > 350) {
+              fall=false
+              this.loc.y=350
+              ground=400
+            }
           }
         }
       }
@@ -307,17 +375,15 @@ class Mug {
         jump=false
         fallDist*=-1
       }
-     }//print(this.loc.y, ground, jump, fall)
-     if (this.loc.y <= ground-50 && !jump && !fall) {
+    }//print(this.loc.y, ground, jump, fall)
+    if (this.loc.y <= ground-50 && !jump && !fall) {
       for (let i=0; i<blocks.length; i++) {
-      if ((this.loc.x <= blocks[i].x-49 &&this.loc.x>=blocks[i].x-50 && keyIsDown(65)) || this.loc.x >= blocks[i].x+blocks[i].w-1 && this.loc.x <= blocks[i].x+ blocks[i].w && keyIsDown(68)) {
-        fall = true
-        break
+        if ((this.loc.x <= blocks[i].x-49 &&this.loc.x>=blocks[i].x-50 && keyIsDown(65)) || this.loc.x >= blocks[i].x+blocks[i].w-1 && this.loc.x <= blocks[i].x+ blocks[i].w && keyIsDown(68)) {
+          fall = true
+          break
+        }
       }
     }
-    
-  }
-
 }
 
   draw() {
@@ -374,7 +440,6 @@ class Enemy {
       if (!this.attacking) {
         enemyMove[this.currentFrame].resize(enemySizeX[level], enemySizeY[level]);
         if (this.deltaX > 0) {
-        
           scale(-1, 1);
           image(enemyMove[this.currentFrame], -this.loc.x - enemySizeX[level], this.loc.y);
         } else {
@@ -416,22 +481,27 @@ class Enemy {
   }
 
   attack() {
+    
     if (frameCount % 30 == 0) this.currentFrame += 1;
     if (this.currentFrame > 1) {
       this.currentFrame = 0;
       this.attacking = false;
     }
     if (this.currentFrame == 1) {
+      //console.log('attack')
       if (this.deltaX > 0) {
-        this.bounds.x = this.loc.x+75;
+        this.bounds.x = this.loc.x;
         this.bounds.y = this.loc.y;
-        this.bounds.w = 100;
+        this.bounds.w = enemySizeX[level];
       } else {
-        this.bounds.x = this.loc.x+25;
+        this.bounds.x = this.loc.x;
         this.bounds.y = this.loc.y;
-        this.bounds.w = 120;
+        this.bounds.w = enemySizeX[level];
       }
+      
       if (frameCount % 30 == 0 && this.bounds.collide(mug.loc.x, mug.loc.y, 50, 50)) {
+        console.log('mode',mode,',level',level)
+        // print(damage[level][mode])
         mug.health -= damage[level][mode];
       }
     }
@@ -440,20 +510,30 @@ class Enemy {
 }
 
 class Bound {
-  constructor(x, y, w, h) {
+  constructor(x, y, w, h, type = "Block") {
     this.x = x;
     this.y = y;
     this.w = w;
     this.h = h;
+    this.type = type;
   }
 
   collide(x, y, w, h) {
-    return (
-      this.x <= x + w &&
-      this.x + this.w >= x &&
-      this.y <= y + h &&
-      this.y + this.h >= y
-    );
+    if(w==50 || w==60){
+      return (
+        this.x <= x + w &&
+        this.x + this.w >= x &&
+        this.y <= y + h &&
+        this.y + this.h >= y
+      );
+    }else{
+     return (
+        this.x <= x + w &&
+        this.x + this.w+1 >= x &&
+        this.y <= y + h &&
+        this.y + this.h >= y
+      );
+    }
   }
 }
 
@@ -483,6 +563,7 @@ class Coin {
     this.h = this.d/2;
     this.bounds = new Bound(this.x, this.y, this.w, this.h);
   }
+
   show() {
     if (this.isAlive == true) {
       strokeWeight(3);
@@ -493,6 +574,7 @@ class Coin {
     }
   }
 }
+
 
 function mouseClicked() {
   if (mug.attackCooldown == 0) {
